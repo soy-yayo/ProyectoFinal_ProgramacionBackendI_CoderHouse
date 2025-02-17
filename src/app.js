1188e07a20ed8763b9d9a7c44a56a5e14786c23d
Yayo <yayomar211@gmail.com>
2025-02-16 19:42:45 -0600

    Finalizamos 'Productos agregados hasta ahora'

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import __dirname from './__dirname.js';
-import handlebasr from 'express-handlebars';
+import __dirname from './utils.js';
+import handlebars from 'express-handlebars';
 import viewsRouter from './routes/views.router.js';
 import { Server } from 'socket.io';
 
@@ -16,8 +16,8 @@ const httpServer = app.listen(PORT, () => console.log(`Server running on port ${
 
 const io = new Server(httpServer);
 
-app.engine('handlebars' , Handlebars.engine());
-app.set('views', __dirname + './views');
+app.engine('handlebars' , handlebars.engine());
+app.set('views', __dirname + '/views');
 app.set('view engine', 'handlebars');
 app.use(express.static(__dirname + 'public'));
 app.use('/', viewsRouter);
--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -1,10 +1,13 @@
 import { Router } from "express";
 import fs from "fs";
+import __dirname from "../utils.js";
 
 const viewsRouter = Router();
 
 viewsRouter.get("/", async (req, res) => {
-  const products = await JSON.parse(fs.readFileSync("products.json", "utf-8"));
-  res.render("index", { title: "Productos agregados hasta el momento", products });
+  const products = await JSON.parse(fs.readFileSync(__dirname + "/data/products.json", "utf-8"));
+  res.render("home", { products });
 });
 
+
+export default viewsRouter;
