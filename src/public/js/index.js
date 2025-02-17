e215fa9449c5996972f043b1cd673ccca33a1b44
Yayo <yayomar211@gmail.com>
2025-02-16 22:43:02 -0600

    Update lógica RealTimeProducts

--- src/public/js/index.js
+++ src/public/js/index.js
@@ -1,20 +1,23 @@
-import __dirname from "../utils.js";
-
-
 const productList = document.getElementById('productList');
 const form = document.getElementById('form');
-
 const socket = io();
+
+Swal.fire({
+  title: 'Bienvenido',
+  text: 'Conectado al servidor de WebSockets',
+  icon: 'success',
+  confirmButtonText: 'Continuar'
+});
 
 socket.on('products', (data) => {
   productList.innerHTML = '';
   data.forEach(product => {
     let li = document.createElement('li');
     li.innerHTML += `
-        <h2>${product.name}</h2>
-        <p>${product.price}</p>
-        <img src="${product.thumbnail}" alt="${product.name}">
-      <button onclick="deleteProduct('${product.id}')">Eliminar</button>
+    <h2>${product.name}</h2>
+      <p>Precio: $${product.price}</p>
+      <img src="${product.thumbnail}" alt="${product.name}" class="w-38 h-40">
+      <button class="border border-grey-600" onclick="deleteProduct(${product.id})">Eliminar</button>
     `;
     productList.appendChild(li);
   });
@@ -24,26 +27,26 @@ socket.on('products', (data) => {
 form.addEventListener('submit', (e) => {
   e.preventDefault();
   // Obtener datos del formulario
-  const name = document.getElementById("title").value.trim();
+  const name = document.getElementById("name").value.trim();
   const price = parseFloat(document.getElementById("price").value);
-  const url = document.getElementById("url").value.trim();
-
+  const thumbnail = document.getElementById("thumbnail").value.trim();
+  
   // Validación de datos
-  if (!name || isNaN(price) || price <= 0 || !url) {
+  if (!name || isNaN(price) || price <= 0 || !thumbnail) {
     Swal.fire("Error", "Por favor, completa todos los campos correctamente.", "error");
     return;
   }
-
+  
   // Crear objeto producto y enviarlo al servidor
   const id = Date.now();
-  const product = { id, title, price, url };
+  const product = { id, name, price, thumbnail};
   socket.emit("newProduct", product);
-
+  
   // Mostrar alerta de éxito y limpiar formulario
   Swal.fire("Éxito", "Producto agregado correctamente.", "success");
   form.reset();
 });
 
 function deleteProduct(id) {
-  socket.emit('delete-product', id);
+  socket.emit('deleteProduct', id);
 }
