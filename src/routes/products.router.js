56505bb7f09b5bc18c29161bf5effc92aa6979cb
Yayo <yayomar211@gmail.com>
2025-02-28 10:03:34 -0600

    Update productsRouter

--- src/routes/products.router.js
+++ src/routes/products.router.js
@@ -65,7 +65,7 @@ productsRouter.get("/", async (req, res) => {
 // Buscar producto por id
 productsRouter.get('/:pid', async (req, res) => {
   const { pid } = req.params;
-  const product = await productModel.find({id : pid});
+  const product = await productModel.findById(pid);
   if (product) {
     res.send({ product });
   } else {
@@ -74,7 +74,7 @@ productsRouter.get('/:pid', async (req, res) => {
 });
 
 // Crear un nuevo producto
-routerProducts.post('/', async (req, res) => {
+productsRouter.post('/', async (req, res) => {
   const { name, price, category, thumbnail, stock,  id} = req.body;
   id = Math.floor(Math.random() * 1000);
 
@@ -91,6 +91,59 @@ routerProducts.post('/', async (req, res) => {
     res.json({ success: "Producto agregado", product: newProduct });
   } catch (error) {
     res.status(500).json({ error: "Error al guardar el producto" });
+  }
+});
+
+productsRouter.put('/:pid', async (req, res) => {
+  const { pid } = req.params; // pid es el id del producto
+  const product = productModel.find({id : pid}); // Buscar el producto por id
+  if (!product) {
+    res.status(404).send({ error: 'Producto no encontrado' });
+    return;
+  }
+  const { name, price, category, thumbnail, stock} = req.body;
+  
+  try {
+    const product = await productModel.findById(pid);
+
+    if (!product) {
+      return res.status(404).send({ error: 'Producto no encontrado' });
+    }
+
+    // Actualizar el producto con los nuevos datos
+    product.name = name || product.name;
+    product.price = price || product.price;
+    product.category = category || product.category;
+    product.thumbnail = thumbnail || product.thumbnail;
+    product.stock = stock || product.stock;
+
+    // Guardar los cambios en la base de datos
+    await product.save();
+
+    // Responder con el producto actualizado
+    res.json({ success: "Producto actualizado", product });
+  } catch (e) {
+    res.status(500).json({ error : e });
+  }
+});
+
+//La ruta DELETE /:pid deberá eliminar el producto con el pid indicado. 
+productsRouter.delete('/:pid', async (req, res) => {
+  const { pid } = req.params;
+
+  try {
+  const product = await productModel.findById(pid);
+  if (!product) {
+    res.status(404).send({ error: 'Producto no encontrado' });
+    return;
+  }
+
+  // Eliminar el producto
+  await productModel.findByIdAndDelete(pid);
+
+  res.send({ success: 'Producto eliminado con éxito' });
+  } catch (e) {
+    res.status(500).send({ error : e });
   }
 });
 
