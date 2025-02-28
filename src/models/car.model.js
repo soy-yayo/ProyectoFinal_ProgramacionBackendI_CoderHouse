ac03628b565a00618ac4e4a4945b0884b7f7c2a7
Yayo <yayomar211@gmail.com>
2025-02-28 10:57:31 -0600

    Creamos carModel

---
+++ src/models/car.model.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+import mongoosePaginate from 'mongoose-paginate-v2';
+
+const cartCollection = 'carrito Quaker';
+
+const carSchema = mongoose.Schema({
+  products : {
+    type :[
+      {
+        product : {
+          type : mongoose.Schema.Types.ObjectId,
+          ref : "product.model",
+          quantity : Number
+        }
+      }
+    ],
+    default : []
+  }
+});
+
+carSchema.plugin(mongoosePaginate);
+export const productModel = mongoose.model(cartCollection, carSchema);
